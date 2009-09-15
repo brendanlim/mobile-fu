79c5535f8a76e98370bbea158fd137d866228870
Brendan G. Lim <brendangl@gmail.com>
2009-09-15 10:57:13 -0400

    Modifying user agent string check to fix Opera issue

--- lib/mobile_fu.rb
+++ lib/mobile_fu.rb
@@ -10,7 +10,7 @@ module ActionController
                           'samsung|sanyo|sharp|telit|tsm|mobile|mini|windows ce|smartphone|' +
                           '240x320|320x320|mobileexplorer|j2me|sgh|portable|sprint|vodafone|' +
                           'docomo|kddi|softbank|pdxgw|j-phone|astel|minimo|plucker|netfront|' +
-                          'xiino|mot-v|mot-e|portalmmm|sagem|sie-s|sie-m|android|ipod|pre'
+                          'xiino|mot-v|mot-e|portalmmm|sagem|sie-s|sie-m|android|ipod'
     
     def self.included(base)
       base.extend(ClassMethods)
